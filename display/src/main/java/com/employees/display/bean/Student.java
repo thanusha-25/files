095ace15f13773221c0d1c4af6ff8febe6403187
thanusha-25 <47080586+thanusha-25@users.noreply.github.com>
2020-06-28 11:12:46 +0530

    student repositiry crud operations(include errors)

---
+++ display/src/main/java/com/employees/display/bean/Student.java
@@ -0,0 +1,54 @@
+//package com.employees.display.bean;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import org.hibernate.annotations.Proxy;
+//
+//@Entity
+//@Table(name="student")
+//@Proxy(lazy=false)
+//public class Student {
+//	@Id
+//	@Column(name="id")
+// private int id;
+//	@Column(name="name")
+// private String name;
+//	@Column(name="email")
+// private String email;
+//	public Student() {
+//		super();
+//	}
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	@Override
+//	public String toString() {
+//		return "Student [id=" + id + ", name=" + name + ", email=" + email + "]";
+//	}
+//	public Student(int id, String name, String email) {
+//		super();
+//		this.id = id;
+//		this.name = name;
+//		this.email = email;
+//	}
+//	
+//
+//}
---
+++ display/src/main/java/com/employees/display/repo/StudentRepository.java
@@ -0,0 +1,11 @@
+//package com.employees.display.repo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.employees.display.bean.Student;
+//
+//@Repository 
+//public interface StudentRepository extends JpaRepository<Student, Integer> {
+//
+//}
