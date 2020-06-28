5fa038af66d7409910d877b316e6df60b2f4d5ee
thanusha-25 <47080586+thanusha-25@users.noreply.github.com>
2020-06-28 11:49:44 +0530

    crud for zemoso ass. using JPA repository

---
+++ StandardDataThymeLeaf-changes/src/main/java/com/jpa/StandardJPA/StandardJpaApplication.java
@@ -0,0 +1,12 @@
+package com.jpa.StandardJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardJpaApplication.class, args);
+	}
+}
